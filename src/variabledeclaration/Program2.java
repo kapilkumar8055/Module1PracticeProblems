8c10ce7522e53f18024527fff743abd093e6dfa4
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2019-09-04 22:22:56 +0530

    final commit

---
+++ src/variabledeclaration/Program2.java
@@ -0,0 +1,22 @@
+package variabledeclaration;
+
+public class Program2 {
+    public static void main(String[] args) {
+        int i = 78;
+        long l = 885569L;
+        byte b = 120;
+        short s = 6389;
+        boolean bl = true;
+        char a = 65;
+        float f = 736.9f;
+        double d = 89.99484;
+        System.out.println("int " + i);
+        System.out.println("long " + l);
+        System.out.println("byte " + b);
+        System.out.println("short " + s);
+        System.out.println("boolean " + bl);
+        System.out.println("char " + a);
+        System.out.println("float " + f);
+        System.out.print("double " + d);
+    }
+}

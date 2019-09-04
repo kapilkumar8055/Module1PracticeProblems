fc1c04db1974f31f0edc945c470d80756f55909f
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2019-09-04 23:09:40 +0530

    final commit

---
+++ src/variabledeclaration/Program13.java
@@ -0,0 +1,10 @@
+package variabledeclaration;
+
+public class Program13 {
+    public static void main(String[] args) {
+        String name = "KAPIL KUMAR";
+        String email = "nauhwar786@gmail.com";
+        long mob = 9634846619L;
+        System.out.println("Name -" + name + "\nMob -" + mob + "\nEmail -" + email);
+    }
+}

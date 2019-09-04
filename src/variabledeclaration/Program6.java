21e3b3686400ece6449c98108fbcdaf0f1d30907
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2019-09-04 22:40:26 +0530

    final commit

---
+++ src/variabledeclaration/Program6.java
@@ -0,0 +1,10 @@
+package variabledeclaration;
+
+public class Program6 {
+    public static void main(String[] args) {
+        double input1 = 3.14;
+        double input2 = 5.0;
+        double total = input1 + input2;
+        System.out.println("the sum of two variables =" + total);
+    }
+}

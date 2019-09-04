ecc319cff9f72f5d11564b3186f3e7a579970d10
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2019-09-04 21:55:23 +0530

    final commit

---
+++ src/variabledeclaration/Program1.java
@@ -0,0 +1,9 @@
+package variabledeclaration;
+
+public class Program1 {
+    public static void main(String[] args) {
+        int input1 = 20;
+        int input2 = 30;
+        System.out.print("input1 = " + input1 + "\ninput2 = " + input2);
+    }
+}
